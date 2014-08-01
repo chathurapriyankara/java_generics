518db06f10c5742da6dd3b82aa9912ff3ec9b935
chathurapriyankara <priyankarahac@gmail.com>
2014-08-01 12:25:37 +0530

    implementations for generic stack class

--- src/generics/Generics.java
+++
@@ -1,9 +0,0 @@
-package generics;
-
-public class Generics {
-
-    public static void main(String[] args) {
-        
-    }
-    
-}
---
+++ src/generics/GenricStackDemo.java
@@ -0,0 +1,52 @@
+package generics;
+
+public class GenricStackDemo {
+
+    public static void main(String[] args) {
+        GenericStack<String> stack = new GenericStack<>();
+        
+        //Add elements to the stack
+        stack.push("Element 1");
+        stack.push("Element 2");
+        stack.push("Element 3");
+        stack.push("Element 4");
+        stack.push("Element 5");
+        
+        System.out.println("Stack size : "+stack.getSize());
+        
+        
+    }
+
+}
+
+class GenericStack<E> {
+
+    private java.util.ArrayList<E> list = new java.util.ArrayList<E>();
+
+    //return stack size
+    public int getSize() {
+        return list.size();
+    }
+
+    //Return top element of the stack
+    public E peek() {
+        return list.get(getSize() - 1);
+    }
+
+    //Remove top element
+    public E pop() {
+        E o = list.get(getSize() - 1);
+        list.remove(o);
+        return o;
+    }
+
+    //Push items to the satck
+    public void push(E o) {
+        list.add(o);
+    }
+
+    //Check stack empty
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+}
